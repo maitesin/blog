ed8a6a3a9c5c52e5d4c7e2e487969203bc94d4c3
Maitesin <violador.de.segmentos@gmail.com>
2016-01-29 11:47:21 +0000

    Fixed the problem with the array

--- clang_tools_2016_01_29/modernize_example/src/old.cpp
+++ clang_tools_2016_01_29/modernize_example/src/old.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 int main(void) {
-	int[] hardcoded = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int hardcoded[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	std::vector<int> v (hardcoded, hardcoded + sizeof(hardcoded) /
 			    sizeof(int));
 	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
