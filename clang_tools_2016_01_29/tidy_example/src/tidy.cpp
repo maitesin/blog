f33ee86b793306e75ad9b5177c86d0a09b6e6fc4
Maitesin <violador.de.segmentos@gmail.com>
2016-01-29 16:30:11 +0000

    Improved code for the tidy

--- clang_tools_2016_01_29/tidy_example/src/tidy.cpp
+++ clang_tools_2016_01_29/tidy_example/src/tidy.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <vector>
 
 int main(void) {
-	std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
-		std::cout << *it << std::endl;
+	bool isGood = true;
+	if (isGood == true) {
+		std::cout << "Is good!" << std::endl;
+	} else {
+		std::cout << "Is not good :(" << std::endl;
 	}
 	return 0;
 }
