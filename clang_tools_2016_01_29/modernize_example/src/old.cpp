f7a3a2c0a498c6c6a14343d6160adfc39ed6d430
Maitesin <violador.de.segmentos@gmail.com>
2016-01-29 11:41:58 +0000

    More old stuff to convert.

--- clang_tools_2016_01_29/modernize_example/src/old.cpp
+++ clang_tools_2016_01_29/modernize_example/src/old.cpp
@@ -3,16 +3,9 @@
 
 int main(void) {
 	std::vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
-	v.push_back(5);
-	v.push_back(6);
-	v.push_back(7);
-	v.push_back(8);
-	v.push_back(9);
-	v.push_back(10);
+	for (size_t i = 1; i <= 10; ++i) {
+		v.push_back(i);
+	}
 	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
 		std::cout << *it << std::endl;
 	}
